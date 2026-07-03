f32e2c0e67cc5564e53e18a64d9dd52f281a474a
aceza <aceza@users.noreply.github.com>
2026-07-03 21:03:33 +0300

    Add contract address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Edit these values when your token is live
 window.UNCLE_SAM_CONFIG = {
-  contractAddress: "TBA — PASTE YOUR CA HERE",
+  contractAddress: "GhFFGKMA1iJD5ibmRRuhZPSPhCrU5TmyNcFqwTygbonk",
   dexUrl: "#",
   chartUrl: "#",
   bannerImage: "assets/banner.png",
