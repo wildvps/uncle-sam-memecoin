b660159738dae4352f472095a380676bd0628e66
aceza <aceza@users.noreply.github.com>
2026-07-04 22:34:02 +0300

    Remove CA

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Edit these values when your token is live
 window.UNCLE_SAM_CONFIG = {
-  contractAddress: "GhFFGKMA1iJD5ibmRRuhZPSPhCrU5TmyNcFqwTygbonk",
+  contractAddress: "TBA — PASTE YOUR CA HERE",
   dexUrl: "#",
   chartUrl: "#",
   bannerImage: "assets/banner.png",
